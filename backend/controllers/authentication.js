43077fbb3eab5b4cd55e1438a734af0c7c56723b
Samuel FIsher <sammykiy@gmail.com>
2022-06-14 00:16:14 -0600

    console log doesnt work

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -16,7 +17,23 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET,{ id: user.userID})
+        req.session.userId = user.userId
+        res.json({ user: user, token: result.value })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    // console.log(req.session.userId)
+    try {
+        // let user = await User.findOne({
+        //     where: {
+        //         userId: 
+        //     }
+        // })
+        // res.json(user)
+    } catch {
+        res.json(null)
     }
 })
 
